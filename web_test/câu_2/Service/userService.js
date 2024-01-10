12f95249b469f536bbbe1351271cc7f54cb8b080
ybinh07 <yenbinh18082307@gmail.com>
2024-01-10 15:33:38 +0700

    fix_c2

--- web_test/câu_2/Service/userService.js
+++ web_test/câu_2/Service/userService.js
@@ -3,7 +3,7 @@ import { User } from "../Schema/userSchema.js";
 import { database } from "../Database/database.js";
  class UserService{
     async register(payload){
-        const result= await database.user().insertOne(new User(payload))
+        const result= await database.user().insertOne(new User(payload));
         return true
     }
 }
